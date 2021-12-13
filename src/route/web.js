d1374f63121ba41c093fa69f3d077d624e504788
Nguyễn Thảo <nguyenthao@Nguyens-MacBook-Pro.local>
2021-12-14 00:14:05 +0700

    test git with dev01

--- src/route/web.js
+++ src/route/web.js
@@ -1,5 +1,6 @@
 import express from "express";
 import homeController from "../controllers/homeController";
+import userController from "../controllers/userController"
 
 let router = express.Router();
 
@@ -13,6 +14,10 @@ let initWebRoutes = (app) => {
     router.post('/put-crud', homeController.putCRUD);
     router.get('/delete-crud', homeController.deleteCRUD);
     
+    //api
+    router.post('/api/login',userController.handleLogin)
+
+
     return app.use("/", router);
 }
 
