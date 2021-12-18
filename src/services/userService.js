ad9cfaa64abd91ab3e86252048d7401a1943ae48
Nguyễn Thảo <nguyenthao@Nguyens-MacBook-Pro.local>
2021-12-18 14:46:27 +0700

    done create user using redux

--- src/services/userService.js
+++ src/services/userService.js
@@ -26,7 +26,7 @@ let handleUserLogin = (email, password) => {
 
                 let user = await db.User.findOne({
                     where: { email: email },
-                    attributes: ['email', 'roleId', 'password'],
+                    attributes: ['email', 'roleId', 'password','firstName','lastName'],
                     raw: true
                 })
                 if (user) {
@@ -126,8 +126,9 @@ let createNewUser = (data)=>{
                 lastName: data.lastName,
                 address: data.address,
                 phoneNumber: data.phoneNumber,
-                gender: data.gender === "1" ? true : false,
+                gender: data.gender,
                 roleId: data.roleId,
+                positionId: data.positionId,
             })
             }
             resolve({
